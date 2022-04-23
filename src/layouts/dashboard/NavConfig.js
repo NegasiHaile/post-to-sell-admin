d2f4cf2d38d120dc2dcc333609284252d198b65a
unknown <you@example.com>
2022-04-23 09:54:12 +0300

    Updated :Blog to advert

--- src/layouts/dashboard/NavConfig.js
+++ src/layouts/dashboard/NavConfig.js
@@ -22,8 +22,8 @@ const navConfig = [
     icon: getIcon('eva:shopping-bag-fill'),
   },
   {
-    title: 'blog',
-    path: '/dashboard/blog',
+    title: 'adverts',
+    path: '/dashboard/adverts',
     icon: getIcon('eva:file-text-fill'),
   },
   // {
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 //
-import Blog from './pages/Blog';
+import Adverts from './pages/Blog';
 import User from './pages/User';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
@@ -22,7 +22,7 @@ export default function Router() {
         { path: 'app', element: <DashboardApp /> },
         { path: 'user', element: <User /> },
         { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> },
+        { path: 'adverts', element: <Adverts /> },
       ],
     },
     {
