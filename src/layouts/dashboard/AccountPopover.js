c526a401348e10c1521a16e69f9cfca725595d9d
unknown <you@example.com>
2022-04-25 13:19:55 +0300

    Added :Logout feature

--- src/layouts/dashboard/AccountPopover.js
+++ src/layouts/dashboard/AccountPopover.js
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 // @mui
 import { alpha } from '@mui/material/styles';
 import { Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton } from '@mui/material';
@@ -31,6 +31,7 @@ const MENU_OPTIONS = [
 // ----------------------------------------------------------------------
 
 export default function AccountPopover() {
+  const navigate = useNavigate();
   const anchorRef = useRef(null);
 
   const [open, setOpen] = useState(null);
@@ -42,7 +43,10 @@ export default function AccountPopover() {
   const handleClose = () => {
     setOpen(null);
   };
-
+  const logOut = () => {
+    localStorage.clear();
+    navigate('/login', { replace: true });
+  };
   return (
     <>
       <IconButton
@@ -101,7 +105,7 @@ export default function AccountPopover() {
 
         <Divider sx={{ borderStyle: 'dashed' }} />
 
-        <MenuItem onClick={handleClose} sx={{ m: 1 }}>
+        <MenuItem onClick={logOut} sx={{ m: 1 }}>
           Logout
         </MenuItem>
       </MenuPopover>
