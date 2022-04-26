7a249765e9d0fbad2c7e81cd8ff2e94450fcb4cf
unknown <you@example.com>
2022-04-26 11:13:15 +0300

    Removed :Unneccessary contents

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 import { Navigate, useRoutes } from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
-import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 //
 import Adverts from './pages/Blog';
 import User from './pages/User';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
-// import Register from './pages/Register';
 import Products from './pages/Products';
 import DashboardApp from './pages/DashboardApp';
 
