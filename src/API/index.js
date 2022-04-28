33c9258d125efa013696faa876140fc2425d1306
Negasi <NegasiHaile.Abadi@gmail.com>
2022-04-28 11:52:27 +0300

    Added :get all adverts api to the index.js which is a center of APIs

--- src/API/index.js
+++ src/API/index.js
@@ -1,5 +1,7 @@
 // Collecting all APIs in to this file
 import { apiAdminSignin } from './Users'; // users APIs
+import { apiGetAllAdverts } from './Adverts'; // Adverts APIs
 
 // Exporting all the APIs
 export { apiAdminSignin }; // users
+export { apiGetAllAdverts }; // Adverts
