30daf5a2fed1a699ca8cd9a6a0ed005c15ba5c1e
Negasi <NegasiHaile.Abadi@gmail.com>
2022-04-29 12:25:55 +0300

    Updated :The locahost server replaced by heroku server

--- src/Constants/Server_Base_URL.js
+++ src/Constants/Server_Base_URL.js
@@ -1,4 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
-export const server = dev ? `http://localhost:${PORT}` : 'https://next-ecommerce-front.vercel.app';
+// export const server = dev ? `http://localhost:${PORT}` : 'https://next-ecommerce-front.vercel.app';
+export const server = dev ? `https://post-to-sell-server.herokuapp.com` : 'https://next-ecommerce-front.vercel.app';
