91e305836a619addc85859677e832ba8360241a3
Negasi <NegasiHaile.Abadi@gmail.com>
2022-05-04 16:25:21 +0300

    Added :An API to delete banner detail

--- src/API/Banners.js
+++ src/API/Banners.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 // Server BASE_URL
 import { server } from '../Constants/Server_Base_URL';
 
+// Token from local storage
+const accesstoken = localStorage.getItem('accesstoken');
 // Get all list of banners (Postcard to be showed on the top of the client app landing page)
 export const apiGetAllBanners = async () => {
   return await axios.get(`${server}/api/banners/list`);
+};
+
+// Delete banner (Postcard to beexport const apiGetAllBanners = async () => {
+export const apiDeleteBanner = async (id) => {
+  return await axios.delete(`${server}/api/banners/delete/${id}`, {
+    headers: {
+      Authorization: accesstoken,
+    },
+  });
 };
--- src/API/index.js
+++ src/API/index.js
@@ -2,10 +2,10 @@
 import { apiAdminSignin, api_getAllUserProducts } from './Users'; // users APIs
 import { apiGetAllProducts } from './Products'; // Products APIs
 import { apiGetAllAdverts } from './Adverts'; // Adverts APIs
-import { apiGetAllBanners } from './Banners'; // Banners API
+import { apiGetAllBanners, apiDeleteBanner } from './Banners'; // Banners API
 
 // Exporting all the APIs
 export { apiAdminSignin, api_getAllUserProducts }; // users
 export { apiGetAllProducts }; // Products
 export { apiGetAllAdverts }; // Adverts
-export { apiGetAllBanners }; // Banners
+export { apiGetAllBanners, apiDeleteBanner }; // Banners
