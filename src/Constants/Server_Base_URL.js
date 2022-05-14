9060e7fbcd24ffded5bc596e22494cf5eea05b9a
Negasi <NegasiHaile.Abadi@gmail.com>
2022-05-14 12:53:45 +0300

    Updated :Server base URL

--- src/Constants/Server_Base_URL.js
+++ src/Constants/Server_Base_URL.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5055;
 
-// export const server = dev ? `http://localhost:${PORT}` : 'https://next-ecommerce-front.vercel.app';
-export const server = dev ? `https://post-to-sell-server.herokuapp.com` : 'https://next-ecommerce-front.vercel.app';
+export const server = dev ? `http://localhost:${PORT}` : 'https://next-ecommerce-front.vercel.app';
+// export const server = dev ? `https://post-to-sell-server.herokuapp.com` : 'https://next-ecommerce-front.vercel.app';
