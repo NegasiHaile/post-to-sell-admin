027b32c8e7448dde3d84643259139f01b6ebd417
Negasi <NegasiHaile.Abadi@gmail.com>
2022-05-25 10:24:30 +0300

    Added the get categories api to the center of APIs

--- src/API/index.js
+++ src/API/index.js
@@ -1,11 +1,13 @@
 // Collecting all APIs in to this file
 import { apiAdminSignin, api_getAllUserProducts } from './Users'; // users APIs
+import { apiGetAllCategories } from './Categories'; // categories
 import { apiGetAllProducts, apiDeleteProduct, apiApproveProduct, apiArchiveProduct } from './Products'; // Products APIs
 import { apiGetAllAdverts, apiApproveAdvert, apiArchiveAdvert, apiDeleteAdvert } from './Adverts'; // Adverts APIs
 import { apiGetAllBanners, apiAddNewBanner, apiEditBanner, apiEditBannerImage, apiDeleteBanner } from './Banners'; // Banners API
 
 // Exporting all the APIs
 export { apiAdminSignin, api_getAllUserProducts }; // users
+export { apiGetAllCategories }; // categories
 export { apiGetAllProducts, apiDeleteProduct, apiApproveProduct, apiArchiveProduct }; // Products
 export { apiGetAllAdverts, apiApproveAdvert, apiArchiveAdvert, apiDeleteAdvert }; // Adverts
 export { apiGetAllBanners, apiAddNewBanner, apiEditBanner, apiEditBannerImage, apiDeleteBanner }; // Banners
